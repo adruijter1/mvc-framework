77384c34c0994da6222a9e26e6a7de1d099d5730
Arjan de Ruijter <rra@mboutrecht.nl>
2022-03-24 06:35:37 +0100

    Begin van de RUD

--- app/views/pages/index.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="css/style.css">
-
-<?php 
-  foreach ($data['users'] as $key => $user) {
-    echo $user->user_id . " | " . $user->user_name . " | " . $user->user_email . " | " . $user->password."<br>";
-  }
-?>
